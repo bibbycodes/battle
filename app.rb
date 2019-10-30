108030b5fa3357a5b1ffa8825ebde1b41828599a
Robert <bibbycodes@gmail.com>
2019-10-30 16:09:19 +0000

    adds redirect for player names

--- app.rb
+++ app.rb
@@ -2,15 +2,24 @@ require 'sinatra/base'
 
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+  post '/names' do # Post
+    # setting the player names into the session hash as an intermediary
+    # Post/Redirect/Get
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play' # Redirect
+  end
+
+  get '/play' do # Get
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
