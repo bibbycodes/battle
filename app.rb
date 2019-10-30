3e9664651932e108e3f19f7efefd3f1ebf553264
Robert <bibbycodes@gmail.com>
2019-10-30 14:26:35 +0000

    Passed first test

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
   get '/index' do
     @name = params[:name]
     erb(:index)
+  end
+
+  get '/' do
+    'Testing infrastructure working!'
   end
 
   run! if app_file == $0
