2ed99e96c195e3345caecaa34d9e7ee5c39e118d
Robert <bibbycodes@gmail.com>
2019-10-31 17:23:16 +0000

    Refactors Model and controller

--- app.rb
+++ app.rb
@@ -12,23 +12,24 @@ class Battle < Sinatra::Base
   post '/names' do # Post
     # setting the player names into the session hash as an intermediary
     # Post/Redirect/Get
-    $game = Game.new
-    $player1 = Player.new(params[:player_1_name])
-    $player2 = Player.new(params[:player_2_name])
+    $game = Game.new(
+      Player.new(params[:player_1_name]), 
+      Player.new(params[:player_2_name])
+    )
     redirect '/play' # Redirect
   end
 
   get '/play' do # Get
-    @player_2_hp = $player2.hp
-    @player_1_name = $player1.name
-    @player_2_name = $player2.name
+    # @player_2_hp = $game.player_2.hp
+    # @player_1_name = $game.player_1.name
+    # @player_2_name = $game.player_2.name
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = $player1.name
-    @player_2_name = $player2.name
-    $game.attack($player2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,19 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player}
   let(:player_2) { double :player}
+
+  describe '#players' do
+    it 'has a player1' do
+      expect(game.player_1).to eq(player_1)
+    end
+
+    it 'has a player1' do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
